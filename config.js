1b349f95678cb93d42e730e36248e6e82154c2ba
Artorius104 <divin.badiabo@gmail.com>
2025-03-04 10:57:31 +0100

    [INIT] Setup repo

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'AKIAWOOXUICMAQR464OC', // TODO
+    secretAccessKey: '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', // TODO
+    endpoint: 's3.eu-west-1.amazonaws.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-80286', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
